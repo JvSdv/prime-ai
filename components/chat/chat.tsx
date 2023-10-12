58dc140df2dec7f6c4e699ebbc52ec8e67e270d3
Joao Victor <jvsdv3@gmail.com>
2023-10-12 14:43:11 -0300

    feat: mostrar chat selecionado

--- components/chat/chat.tsx
+++ components/chat/chat.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { mobileMenuAtom } from "@/atoms/navigation";
 import { ChatWithMessageCountAndSettings } from "@/types/collections";
 import { titleCase } from "@/utils/helpers";
@@ -5,8 +6,12 @@ import { useSetAtom } from "jotai";
 import { MessageCircle, MessageSquare } from "lucide-react";
 import { DateTime } from "luxon";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 const Chat = ({ chat }: { chat: ChatWithMessageCountAndSettings }) => {
+  const pathname = usePathname();
+  const isCurrentChat = pathname === `/chat/${chat.id}`;
+
   const showMobileMenu = useSetAtom(mobileMenuAtom);
   return (
     <Link
@@ -16,9 +21,9 @@ const Chat = ({ chat }: { chat: ChatWithMessageCountAndSettings }) => {
       title={chat.title as string}
       href={`/chat/${chat.id}`}
     >
-      <div className="flex items-center w-full gap-2 px-3 py-2 transition-colors duration-100 ease-in-out rounded-md bg-neutral-100 dark:bg-neutral-900 dark:hover:bg-neutral-800">
+      <div className={`flex items-center w-full gap-2 px-3 py-2 transition-colors duration-100 ease-in-out rounded-md bg-neutral-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 ${isCurrentChat ? 'font-bold' : 'font-normal'}`}>
         <MessageSquare className="shrink-0" size="16" />
-        <div className="text-sm leading-loose line-clamp-1">{chat.title}</div>
+        <div className="text-sm leading-loose line-clamp-1"> {chat.title}</div>
       </div>
       {/* Meta */}
       <div className="flex items-center mt-1 dark:text-neutral-600 text-neutral-400">
